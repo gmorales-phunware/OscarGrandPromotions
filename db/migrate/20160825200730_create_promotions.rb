682a69e1a0f99d94ae420b2ea839e5878b2bd6de
Gabe Morales <gabomorales@me.com>
2016-09-02 16:25:19 -0500

    Initial commit

---
+++ db/migrate/20160825200730_create_promotions.rb
@@ -0,0 +1,13 @@
+class CreatePromotions < ActiveRecord::Migration
+  def change
+    create_table :promotions do |t|
+      t.string :title
+      t.string :title_arabic
+      t.string :price
+      t.string :price_arabic
+      t.string :imageURL
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)W
+#
+#
+# Promotion.create title: "Irish White Cheddar Cheese \ 1Kg", price: "Just $36.00", imageURL: "http://solutions-img.phunware.com/resources/b77981d5-83e5-49c9-abff-d1ed2914bbeb/201644760960660victoria-secret.jpg"
+# Promotion.create title: "Local Bananas \ 1Kg", price: "Just $5.95", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic2.gif"
+# Promotion.create title: "Sinai Canteloupe \ 1Kg", price: "Just $5.75", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic4.gif"
+# Promotion.create title: "Panda Mozzarella", price: "Just $10.75", imageURL: "http://oscarstores.com/images+/homeBody_RestPic3.gif"
+# Promotion.create title: "Chobani Yoghurt", price: "Just $15.75", imageURL: "http://oscarstores.com/images+/homeBody_RestPic1.gif"
+Promotion.create title: "Test English", title_arabic: "Test Arabic", price: "test Price", price_arabic: "test Arabic", imageURL: "http://oscarstores.com/images+/homeBody_RestPic1.gif"
+
+
+
+
+# Promotion.create title: "Sinai Canteloupe \ 1Kg", price: "Just $5.75", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic4.gif"
+# Promotion.create title: "Sinai Canteloupe \ 1Kg", price: "Just $5.75", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic4.gif"
+# Promotion.create title: "Sinai Canteloupe \ 1Kg", price: "Just $5.75", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic4.gif"
+# Promotion.create title: "Sinai Canteloupe \ 1Kg", price: "Just $5.75", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic4.gif"
+# Promotion.create title: "Sinai Canteloupe \ 1Kg", price: "Just $5.75", imageURL: "http://www.oscarstores.com/images+/homeBody_RestPic4.gif"
+
